9bc03c4e6e72eded12f7c612560d7832e37e1c69
Trobrynn <50403257+Trobrynn@users.noreply.github.com>
2020-03-12 11:24:07 +0800

    Update talker.py

--- homework-3rd/code/talker.py
+++ homework-3rd/code/talker.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python
-# license removed for brevity
+
 import rospy
 from std_msgs.msg import String
 
-def talker(t):
+def talker(number):
     pub = rospy.Publisher('chatter', String, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(1) # 1hz
     while not rospy.is_shutdown():
-        #hello_str = "1711515 %s" % rospy.get_time()
-	hello_str = "1711515 %d" % t
-	t+=1
+	hello_str = "1711515 %d" % number
+	number+=1
         rospy.loginfo(hello_str)
         pub.publish(hello_str)
         rate.sleep()
 
 if __name__ == '__main__':
-    t=1
+    number=1
     try:
-        talker(t)
+        talker(number)
     except rospy.ROSInterruptException:
         pass
 
