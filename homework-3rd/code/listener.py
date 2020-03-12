ae6eba6db2ab048d1333cf7ef126dd136ac47c71
Trobrynn <50403257+Trobrynn@users.noreply.github.com>
2020-03-12 11:32:22 +0800

    Update listener.py

--- homework-3rd/code/listener.py
+++ homework-3rd/code/listener.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 import rospy
 from std_msgs.msg import String
 
@@ -7,16 +8,10 @@ def callback(data):
     
 def listener():
 
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
     rospy.init_node('listener', anonymous=True)
 
     rospy.Subscriber("chatter", String, callback)
 
-    # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
 if __name__ == '__main__':
